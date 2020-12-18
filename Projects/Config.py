6bf00aefe962514fb5b3e3c6bf5fab048ca46150
FlyInSpace <55352701+PhanudetFlyInSpace@users.noreply.github.com>
2020-12-18 13:10:19 +0700

    Delete Config.py

--- Projects/Config.py
+++
@@ -1,3 +0,0 @@
-Channel_secret  = "435eaec5a712f73b74a43f3897af472d"
-Channel_access_token = "TzglDAfUMSry1sEXmIgpJZSoj6lecBw1z1q9DJEXzMBEJWEQwkHGGzhKO7pLViQNEkB8dKIyrsZQIacQcYb2/CmM2ibGoNzVFwiZY23gJwqowLzYSfIMvP+uy5J/qYnuI7/zGwjBeh2a0hddPAQHrAdB04t89/1O/w1cDnyilFU="
-Bot_basic_ID = "@038iozqi"
